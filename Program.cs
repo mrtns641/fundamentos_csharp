231115578d6ae89e7c56ed21aad7139acb1478e3
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-05-15 00:49:38 -0300

    Estrutura de repetição
    Exemplos das estruturas de repetição for, while e foreach

--- Program.cs
+++ Program.cs
@@ -1,27 +1,16 @@
-﻿Console.WriteLine("Digite uma opção de 1 a 3");
-var opcao = Console.ReadLine();
+﻿var valores = Console.ReadLine();
+var valoresArray = valores.Split(" ");
 
-if (opcao == "1") {
-    Console.WriteLine("Você entrou no menu de Cadastro");
-} else if (opcao == "2") {
-    Console.WriteLine("Você entrou no menu de Reclamação");
-} else if (opcao == "3") {
-    Console.WriteLine("Você entrou no menu de Atendimento de Suporte");
-} else {
-    Console.WriteLine("Opção inválida");
+for (var i = 0; i < valoresArray.Length; i++) {
+    Console.Write(valoresArray[i] + " ");
 }
 
-switch (opcao){
-    case "1":
-        Console.WriteLine("Você entrou no menu de Cadastro");
-        break;
-    case "2":
-        Console.WriteLine("Você entrou no menu de Reclamação");
-        break;
-    case "3": 
-        Console.WriteLine("Você entrou no menu de Atendimento de Suporte");
-        break;    
-    default:
-        Console.WriteLine("Opção inválida");
-        break;
+var contador = 0;
+while(contador < valoresArray.Length) {
+    Console.Write(valoresArray[contador] + " ");
+    contador++;
+}
+
+foreach (var item in valoresArray){
+    Console.Write(item + " ");
 }
