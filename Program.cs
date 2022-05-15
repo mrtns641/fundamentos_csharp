bd659d35d7c1b13f7879b85b880baa02d75dfa6d
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-05-15 00:30:31 -0300

    Estrutura condicional
    Exemplos das estruturas condicionais if-else e switch case

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Digite uma opção de 1 a 3");
+var opcao = Console.ReadLine();
 
-Console.WriteLine("Digite o seu nome: ");
+if (opcao == "1") {
+    Console.WriteLine("Você entrou no menu de Cadastro");
+} else if (opcao == "2") {
+    Console.WriteLine("Você entrou no menu de Reclamação");
+} else if (opcao == "3") {
+    Console.WriteLine("Você entrou no menu de Atendimento de Suporte");
+} else {
+    Console.WriteLine("Opção inválida");
+}
 
-var nome = Console.ReadLine();
-//tipo explicito
-//string nome1 = Console.ReadLine();
-
-//Console.WriteLine(nome);
-//Console.WriteLine("Olá, " + nome);
-//interpolação
-Console.WriteLine($"Olá, {nome}");
-
-var umaString = "Essa é uma string";
-var numeroInt = 5;
-var numeroFloat = 5.4f;
-var numeroDouble = 5.4;
-var numeroDecimal = 5.3m;
-var array = new int[3] { 1, 2, 3};
-var caractere = 'a';
-var date = new DateTime(1990, 1, 1);
+switch (opcao){
+    case "1":
+        Console.WriteLine("Você entrou no menu de Cadastro");
+        break;
+    case "2":
+        Console.WriteLine("Você entrou no menu de Reclamação");
+        break;
+    case "3": 
+        Console.WriteLine("Você entrou no menu de Atendimento de Suporte");
+        break;    
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
