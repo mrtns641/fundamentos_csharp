a6bec6e02739501788375fa47c47d0e5654566be
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-05-15 01:03:50 -0300

    Listas e LINQ
    Exemplo de Lista e alguns métodos populares do LINQ

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
-﻿var valores = Console.ReadLine();
-var valoresArray = valores.Split(" ");
+﻿var notas = new List<int> { 10, 5, 3, 2, 10, 4, 5, 6, 8, 2};
 
-for (var i = 0; i < valoresArray.Length; i++) {
-    Console.Write(valoresArray[i] + " ");
-}
+var anyNota1 = notas.Any(n => n == 1);
+var FirstNota10 = notas.First(n => n == 10);
+var singleNota8 = notas.Single(n => n == 8);
+var orderedNotas = notas.OrderBy(n => n);
+var max = notas.Max();
+var min = notas.Min();
+var sum = notas.Sum();
+var average = notas.Average();
 
-var contador = 0;
-while(contador < valoresArray.Length) {
-    Console.Write(valoresArray[contador] + " ");
-    contador++;
-}
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Average: {average}");
 
-foreach (var item in valoresArray){
-    Console.Write(item + " ");
+foreach (var nota in orderedNotas){
+    Console.Write(nota + " ");    
 }
